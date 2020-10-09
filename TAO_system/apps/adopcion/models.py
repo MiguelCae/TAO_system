73b10569109236c73eea45f0f97d6fb809dc4059
Miguel <miguelmakio61@gmail.comgit config --global user.email miguelmakio61@gmail.com;git config --global user.email miguelmakio61@gmail.com>
2020-10-08 22:37:44 -0500

    Se realiza conexion bd, se crean dos modelos con sus respetivas tabalas migradas a mysql

--- TAO_system/apps/adopcion/models.py
+++ TAO_system/apps/adopcion/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 
 # Create your models here.
+
+class persona(models.Model):
+    documento = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    telefono = models.IntegerField()
+    correo = models.EmailField(max_length=80)
