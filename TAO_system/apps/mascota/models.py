86b5e203785990419df1fbc50f5f6d54fccb8e00
Miguel <miguelmakio61@gmail.comgit config --global user.email miguelmakio61@gmail.com;git config --global user.email miguelmakio61@gmail.com>
2020-10-11 16:29:26 -0500

    Se crea app de usuarios y se modifican algunos modelos en la app de mascota

--- TAO_system/apps/mascota/models.py
+++ TAO_system/apps/mascota/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Create your models here.
 class mascota(models.Model):
+    default = ''
     nombre = models.CharField(max_length=50)
+    especie = models.CharField(max_length=50, null= True)
+    raza = models.CharField(max_length=50, null= True)
+    tamaño = models.CharField(max_length=50, null= True)
+    estado = models.CharField(max_length=50, null= True)
     sexo = models.CharField(max_length=10)
     edad_aproximada = models.IntegerField()
     fecha_rescate = models.DateField()
